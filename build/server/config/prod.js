eedbf3aa230fb0df27440f36b3f694d2a2aa3df8
THPTUD <thptud@gmail.com>
2018-08-01 16:25:53 +0700

    fix

---
+++ build/server/config/prod.js
@@ -0,0 +1,6 @@
+// production keys here
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  chainURL: process.env.CHAIN_URL
+};
---
+++ build/server/index.js
@@ -0,0 +1,103 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+// const setupApiRoutes = require('./middlewares/api');
+const logger = require('./logger');
+
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./services/auth');
+const MongoStore = require('connect-mongo')(session);
+const schema = require('./schema/schema');
+const keys = require('./config/keys');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.HTTP_PORT = process.env.HTTP_PORT || 3000;
+
+process.on('unhandledRejection', onUnhandledError);
+process.on('uncaughtException', onUnhandledError);
+
+const setupAppRoutes =
+  process.env.NODE_ENV === 'development' ? require('./middlewares/development') : require('./middlewares/production');
+
+const app = express();
+
+function onUnhandledError(err) {
+  try {
+    logger.error(err);
+  } catch (e) {
+    console.log('LOGGER ERROR:', e); //eslint-disable-line no-console
+    console.log('APPLICATION ERROR:', err); //eslint-disable-line no-console
+  }
+  process.exit(1);
+}
+
+const MONGO_URI = keys.mongoURI;
+
+const options = {
+  server: {socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000}},
+  replset: {socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000}}
+};
+
+// Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
+mongoose.Promise = global.Promise;
+
+// Connect to the mongoDB instance and log a message
+// on success or failure
+mongoose.connect(
+  MONGO_URI,
+  options
+);
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', (error) => console.log('Error connecting to MongoLab:', error));
+
+// Configures express to use sessions.  This places an encrypted identifier
+// on the users cookie.  When a user makes a request, this middleware examines
+// the cookie and modifies the request object to indicate which user made the request
+// The cookie itself only contains the id of a session; more data about the session
+// is stored inside of MongoDB.
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: keys.cookieKey,
+    store: new MongoStore({
+      url: MONGO_URI,
+      autoReconnect: true
+    })
+  })
+);
+
+// Passport is wired into express as a middleware. When a request comes in,
+// Passport will examine the request's session (as set by the above config) and
+// assign the current user to the 'req.user' object.  See also servces/auth.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Instruct Express to pass on any request made to the '/graphql' route
+// to the GraphQL instance.
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
+
+app.set('env', process.env.NODE_ENV);
+logger.info(`Application env: ${process.env.NODE_ENV}`);
+
+app.use(logger.expressMiddleware);
+app.use(bodyParser.json());
+
+// application routes
+//setupApiRoutes(app);
+setupAppRoutes(app);
+
+http.createServer(app).listen(process.env.HTTP_PORT, () => {
+  logger.info(`HTTP server is now running on http://localhost:${process.env.HTTP_PORT}`);
+});

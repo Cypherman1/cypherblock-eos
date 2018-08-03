8b4f40cdbd9d1a789124112feaf56c2e4f2b50cf
THPTUD <thptud@gmail.com>
2018-08-03 23:46:01 +0700

    gzip

--- build/server/middlewares/production.js
+++ build/server/middlewares/production.js
@@ -1,24 +1,27 @@
 const {resolve} = require('path');
-const express = require('express');
+//const express = require('express');
+// const compression = require('compression');
+const expressStaticGzip = require('express-static-gzip');
 
 const clientBuildPath = resolve(__dirname, '..', '..', 'client');
 
 module.exports = function setup(app) {
-  app.get('*.js', function(req, res, next) {
-    req.url += '.gz';
-    res.set('Content-Encoding', 'gzip');
-    res.set('Content-Type', 'text/javascript');
-    next();
-  });
+  // app.use(compression());
+  // app.get('*.js', function(req, res, next) {
+  //   req.url += '.gz';
+  //   res.set('Content-Encoding', 'gzip');
+  //   res.set('Content-Type', 'text/javascript');
+  //   next();
+  // });
 
-  app.get('*.css', function(req, res, next) {
-    req.url += '.gz';
-    res.set('Content-Encoding', 'gzip');
-    res.set('Content-Type', 'text/css');
-    next();
-  });
+  // app.get('*.css', function(req, res, next) {
+  //   req.url += '.gz';
+  //   res.set('Content-Encoding', 'gzip');
+  //   res.set('Content-Type', 'text/css');
+  //   next();
+  // });
 
-  app.use('/', express.static(clientBuildPath));
+  app.use('/', expressStaticGzip(clientBuildPath));
 
   // all other requests be handled by UI itself
   app.get('*', (req, res) => res.sendFile(resolve(clientBuildPath, 'index.html')));
